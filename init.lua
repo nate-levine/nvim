5482c69d3c4db97b5b6f7257879f465ccf989885
nate-levine <n8levine11@gmail.com>
2023-10-20 16:25:59 -0400

    Added config to repository

---
+++ init.lua
@@ -0,0 +1,28 @@
+-- Lazy.vim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({import = "plugins"})
+
+
+-- Set colorscheme
+vim.cmd "colorscheme gruvbox"
+-- Set line numbers
+vim.cmd "set number"
+-- Remove ~ from unused lines
+
+-- Indent rules
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,5 @@
+return
+{
+    "morhetz/gruvbox",
+    priority = 1000, -- make sure to load this before all the other start plugins
+}
